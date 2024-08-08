46effe2c55d6a523661fd5482e3493b2a6cf4eb2
OsorioP7 <thomas.osorio.passos@gmail.com>
2024-08-08 07:57:18 -0500

    commit quiz 1

--- app/src/main/java/quiz1sistemas/App.java
+++ app/src/main/java/quiz1sistemas/App.java
@@ -1,20 +1,88 @@
-
 package quiz1sistemas;
+
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         
-        // Logica de solucion
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ingrese tipo de boleta: \n1: V \n2: G");
+        String TipoDeboleta = sc.nextLine();
         
+        
+        System.out.println("cuantas boletas desea comprar: ");
+        int CantidadBoletas = sc.nextInt();
+        
+        System.out.println("ingrese el numero del combo \n1: 1 \n2: 2 \n3: 3 ");
+        int numeroCombo = sc.nextInt();
+
+        System.out.println("cuantos combos compro: ");
+        int cantidadCombos = sc.nextInt();
+
     }
 
-    /*  Implemente una funcion llamada calcularValorBoleta que cumpla con las siguientes caracteristicas
+
+/*  Implemente una funcion llamada calcularValorBoleta que cumpla con las siguientes caracteristicas
         * Recibe: 
             - Una cadena de caracteres que representa el tipo de silla comprada (V ó G)
             - Un entero que representa la cantidad de boletas compradas
         * Retorna:
             - Un entero representando el valor total de las boletas
     */
+
+
+    public static int calcularValorBoleta(String TipoDeboleta, int CantidadBoletas){
+
+        try {
+int valorBoleta = 0;
+            
+            if (CantidadBoletas > 0) { 
+                
+                switch (TipoDeboleta) {
+                   
+                    case "V":
+
+                        valorBoleta = 15000;
+
+                       break; 
+
+                    case "G": 
+
+                        valorBoleta = 10000;
+                        
+            
+                        break;
+                
+                    default:
+                        System.out.println("no eligio boleta");
+
+                        break;
+
+                        
+            
+                }
+                
+                
+
+            }
+            else {
+                System.out.println("no es valido");
+
+            }
+            int valortotalboletas = CantidadBoletas * valorBoleta;
+
+                return valortotalboletas;
+           
+        } catch (Exception e) {
+
+            return -1;
+        }
+        
+    }
+
+
+    
     //------------------------------------------------------------------
     
 
@@ -30,6 +98,55 @@ public class App {
     */
     //------------------------------------------------------------------
     
+        public static int calcularValorNetoCombo(int numeroCombo, int cantidadCombos){
+
+            int valorcombo = 0;
+            try {
+                if (cantidadCombos > 0 ) {
+
+                    switch (numeroCombo) {
+                        case 1: valorcombo = 40000; 
+                            
+                            break;
+                        case 2: valorcombo = 30000;
+
+                            break; 
+
+                        case 3: valorcombo = 42000;
+
+                        break;
+
+
+                        default:
+
+                            System.out.println("ERROR");
+                            break;
+                    }
+                    
+                }
+
+                else{
+
+                    System.out.println("error");
+                }
+                
+                int valorNetoCombo = valorcombo * cantidadCombos;
+            
+                return valorNetoCombo;
+
+            } catch (Exception e) {
+                
+                return -1;
+
+            }
+
+
+        }
+
+
+
+
+
 
 
     //------------------------------------------------------------------
@@ -37,12 +154,44 @@ public class App {
     /*  Implemente una funcion llamada calcularDescuentoCombo que cumpla con las siguientes caracteristicas
         * Recibe:
             - Una cadena de caracteres que representa el tipo de silla comprada (V ó G)
+            -un entero que representa el tipo de combo 
             - Un entero representando el valor neto del combo
         * Retorna:
             - Un entero representando el valor del descuento
     */
     //------------------------------------------------------------------
     
+        public static int calcularDescuento (String TipoDeboleta, int tipoCombo, int valorNetoCombo ){
+            
+
+            int descuento = 0;
+      try {
+        
+        switch (tipoCombo) {
+            case 1: 
+            
+            descuento = (int)(valorNetoCombo * 0.05);
+                
+                break;
+        
+            default:
+                break;
+        }
+
+
+        return 0;
+
+      } catch (Exception e) {
+        
+        return -1;  
+      }
+
+
+
+
+
+
+
 
 
     //------------------------------------------------------------------
@@ -60,4 +209,6 @@ public class App {
 
 
     //------------------------------------------------------------------
+}
+
 }
